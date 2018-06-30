925a0631a4ee59b369eced06f2066d454fc38a7c
tom.bescherer <tbescherer@gmail.com>
2018-06-30 15:52:36 -0400

    log the session counter and formdata

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request, session
 from twilio.rest import Client
 import os
 app = Flask(__name__)
+app.secret_key = os.environ['REUN_FLASK_SECRET_KEY']
 client = Client(os.environ['REUN_TWILIO_ACCOUNT_SID'], os.environ['REUN_TWILIO_ACCOUNT_SECRET'])
 
 
@@ -11,15 +12,17 @@ def hello_world():
     
 @app.route('/sms', methods=['POST'])
 def text_text():
-    # Create a phone call that uses our other route to play a song from Spotify.
+    session['counter'] = session.get('counter', 0) + 1
+    app.logger.info(session['counter'])
+    app.logger.info(request.form)
     message = client.messages \
                 .create(
                      body="test",
                      from_=os.environ['REUN_TWILIO_FROM_NUMBER'],
                      to='+19173735223'
                  )
- 
-    return str(response)
+    
+    return str('empty')
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
