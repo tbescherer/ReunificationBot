debe69d93a40b8b43e206afc4807f7fbabd34f0d
tom.bescherer <tbescherer@gmail.com>
2018-06-30 15:06:31 -0400

    set up twilio test endpoint

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 from flask import Flask
+from twilio.rest import Client
+import os
 app = Flask(__name__)
+client = Client(os.environ['REUN_TWILIO_ACCOUNT_SID'], os.environ['REUN_TWILIO_ACCOUNT_SECRET'])
+
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    return 'App is live!'
+    
+@app.route('/sms', methods=['POST'])
+def text_text():
+    # Create a phone call that uses our other route to play a song from Spotify.
+    message = client.messages \
+                .create(
+                     body="test",
+                     from_=os.environ['REUN_TWILIO_FROM_NUMBER'],
+                     to='+19173735223'
+                 )
+ 
+    return str(response)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
